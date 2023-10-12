cb0012401a482efab3b83e848c13ae2095832cc1
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-10-13 00:18:34 +0200

    grid, left bar and logo

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1 class="text-red-500">SocialApp</h1>
+  <div :class="{'dark': darkMode}">
+    <div class="bg-white dark:bg-dim-900">
+    <div class="min-h-full">
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8
+        lg:gap-5">
+        <!--left sidebar-->
+        <div class="md:block xs-col-span-1 xl:col-span-2">
+          <div class="sticky top-0"></div>
+            <SidebarLeft/>
+        </div>
+
+
+        <!--main content-->
+        <!--right content-->
+      </div>
+
+    </div>
   </div>
+
+</div>
 </template>
+<script setup>
+const darkMode = ref(false);
+
+</script>
