57f272280336bc542d7bee6da291032d148d709e
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-10-13 16:35:12 +0200

    left sidebar, tab component

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
       <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8
         lg:gap-5">
         <!--left sidebar-->
-        <div class="md:block xs-col-span-1 xl:col-span-2">
+        <div class="hidden md:block xs-col-span-1 xl:col-span-2">
           <div class="sticky top-0"></div>
             <SidebarLeft/>
         </div>
