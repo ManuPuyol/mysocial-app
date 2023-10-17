ee6de0325dd3f6e5752744a7c90b466d91f4f9af
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-10-17 23:39:28 +0200

    login page inputs component and layout

--- app.vue
+++ app.vue
@@ -1,38 +1,34 @@
 <template>
-  <div :class="{'dark': darkMode}">
+  <div :class="{ dark: darkMode }">
     <div class="bg-white dark:bg-dim-900">
-    <div class="min-h-full">
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8
-        lg:gap-5">
-        <!--left sidebar-->
-        <div class="hidden md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0"></div>
-            <SidebarLeft/>
+      <!--App-->
+      <div v-if="false" class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!--left sidebar-->
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0"></div>
+            <SidebarLeft />
+          </div>
+
+          <!--main content-->
+
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view> </router-view>
+          </main>
+          <!--right content-->
+
+          <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0"></div>
+            <SidebarRight />
+          </div>
         </div>
-
-
-        <!--main content-->
-
-        <main class="col-span-12 md:col-span-8 xl:col-span-6">
-          <router-view>
-            
-          </router-view>
-        </main>
-        <!--right content-->
-
-        <div class="hidden col-span-12  md:block xl:col-span-4 md:col-span-3">
-          <div class="sticky top-0"></div>
-            <SidebarRight/>
-        </div>
-
       </div>
-
+      <AuthPage v-else></AuthPage>
     </div>
   </div>
-
-</div>
 </template>
 <script setup>
 const darkMode = ref(false);
-
 </script>
