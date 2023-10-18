93f2967af7d0a42a6b27ddcfadb2510aea304539
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-10-18 21:56:14 +0200

    useAuth, refreshtokens, refresh get endpoint

--- server/db/users.js
+++ server/db/users.js
@@ -17,3 +17,10 @@ export const getUserByUsername = (username) => {
         }
     })
 }
+export const getUserById = (userId) =>{
+    return prisma.user.findUnique({
+        where:{
+            id: userId
+        }
+    })
+}
