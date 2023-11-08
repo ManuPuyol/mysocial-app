39cc46a965c6f1ca7e24c0d9bf077efdc3577774
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-11-08 14:58:09 +0100

    testing

--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -5,10 +5,10 @@ import { userTransformer } from "~/server/transformers/user";
 import { createRefreshToken } from "~/server/db/refreshTokens";
 import { sendError } from "h3";
 export default defineEventHandler(async (event) => {
-  console.log(event)
+  assertMethod(event, ['POST'])
   const body = await readBody(event);
   const { username, password } = body;
-  console.log("username y password: ",username, password)
+  
   if (!username || !password) {
     return sendError(
       event,
