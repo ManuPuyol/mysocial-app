b50f431d9138d64f73225d42b331868fce95cce2
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-11-08 11:36:42 +0100

    testing vercel config

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -19,7 +19,7 @@ export default () => {
     return new Promise(async (resolve, reject) => {
       try {
         const data = await $fetch("/api/auth/login", {
-          method: "POST",
+          method: "post",
           body: {
             username,
             password,
