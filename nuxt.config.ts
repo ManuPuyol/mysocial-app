a7c0b098d3f66719d15344034d6a86d63ac716b5
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-11-14 20:10:18 +0100

    vercel.json configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,7 @@ export default defineNuxtConfig({
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
     cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
   },
+  routeRules: {
+    '/api/**': { cors: true },
+  },
 });
