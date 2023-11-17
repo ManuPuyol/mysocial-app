c4c83d6c3b9b2667997057d5305c886a09ca692a
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-11-17 23:30:22 +0100

    redirects config test

--- server/middleware/redirect.global.js
+++ server/middleware/redirect.global.js
@@ -1,9 +1,12 @@
 // middleware/redirect.js
-export default defineEventHandler (async (event) =>{  
-    const urlObj = getRequestURL(event)
-    console.log(urlObj)
-    if (urlObj.protocol === 'http') {
-        urlObj.protocol = 'https'
-        await sendRedirect(event, urlObj.href)
-      }
-  })
+export default defineEventHandler((event) => {
+  const urlObj = getRequestURL(event);
+  console.log(urlObj);
+  //TODO: Remake this
+  if (urlObj.hostname !== "localhost") {
+    if (urlObj.protocol === "http:") {
+      urlObj.protocol = "https:";
+      sendRedirect(event, urlObj.href);
+    }
+  }
+});
