5fe737a4e7772e41c19b70542cec4bbad66ffb46
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-11-18 16:21:57 +0100

    urlobj deleted by error

--- server/middleware/redirect.global.js
+++ server/middleware/redirect.global.js
@@ -1,13 +1,11 @@
 // middleware/redirect.js
 export default defineEventHandler((event) => {
-
+  const urlObj = getRequestURL(event);
   //TODO: Remake this
   if (urlObj.hostname !== "localhost") {
-   
     if (urlObj.protocol === "http:") {
       urlObj.protocol = "https:";
       sendRedirect(event, urlObj.href);
     }
-   
-  } 
+  }
 });
