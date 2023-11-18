756a515f5e36cdf4f6be7132f187f4a5fe16df39
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-11-18 16:09:01 +0100

    reconfig for new domains

--- server/middleware/redirect.global.js
+++ server/middleware/redirect.global.js
@@ -1,9 +1,6 @@
 // middleware/redirect.js
 export default defineEventHandler((event) => {
-  /* const urlObj = getRequestURL(event);
-  console.log(urlObj);
-  urlObj.host = 'www.' + urlObj.host;
-  console.log(urlObj.host);
+
   //TODO: Remake this
   if (urlObj.hostname !== "localhost") {
    
@@ -11,10 +8,6 @@ export default defineEventHandler((event) => {
       urlObj.protocol = "https:";
       sendRedirect(event, urlObj.href);
     }
-    if(!urlObj.host.startsWith('www.')){
-        console.log("holaa")
-        urlObj.host = 'www.' + urlObj.host;
-        sendRedirect(event, urlObj.href);
-    }
-  } */
+   
+  } 
 });
