fbe6f65cb16eaf7c696aee5106f5f9f435efdf11
ManuPuyol <manuelpuyol.dev@gmail.com>
2023-11-18 11:56:09 +0100

    domain problems

--- server/middleware/redirect.global.js
+++ server/middleware/redirect.global.js
@@ -1,16 +1,20 @@
 // middleware/redirect.js
 export default defineEventHandler((event) => {
-  const urlObj = getRequestURL(event);
+  /* const urlObj = getRequestURL(event);
   console.log(urlObj);
+  urlObj.host = 'www.' + urlObj.host;
+  console.log(urlObj.host);
   //TODO: Remake this
   if (urlObj.hostname !== "localhost") {
+   
     if (urlObj.protocol === "http:") {
       urlObj.protocol = "https:";
       sendRedirect(event, urlObj.href);
     }
     if(!urlObj.host.startsWith('www.')){
+        console.log("holaa")
         urlObj.host = 'www.' + urlObj.host;
         sendRedirect(event, urlObj.href);
     }
-  }
+  } */
 });
